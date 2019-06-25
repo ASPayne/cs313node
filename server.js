6f64a57318aca4431fa18ba240b47d5ff7d47be6
aspayne <pay12008@byui.edu>
2019-06-25 16:02:41 -0600

    attempt to access db on heroku

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-require('dotenv').config();
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+require("dotenv").config();
 const connectionString = process.env.DATABASE_URL;
 
+const pool = new Pool({ connectionString: connectionString });
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express();
+
+app.use(express.static("public"));
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+  console.log("recived a request for /");
+
+  res.write("sending back root");
+  res.end();
+});
+
+app.get("/home", function() {
+  console.log("recived a request for the home page");
+});
+
+app.get("/getPerson", function(req, res) {
+
+    var sql = "SELECT first_name FROM person where id = $1";
+
+
+    pool.query(sql, [req], function(err, result) {
+        // If an error occurred...
+        if (err) {
+          console.log("Error in query: ");
+          console.log(err);
+        }
+      
+        // Log this to the console for debugging purposes.
+        console.log("Back from DB with result:");
+        console.log(result.rows);
+        res.send(result.rows);
+      })
+
+});
