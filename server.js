de7146670fce0e1ef7e572a16c783c93bedf1738
aspayne <pay12008@byui.edu>
2019-07-01 16:38:38 -0600

    updates to node

--- server.js
+++ server.js
@@ -1,18 +1,5 @@
 const express = require("express");
-var router = express.Router();
-const pg = require('pg')
-var conString = "postgres://qofbggsapgbgtx:362d864eeb0f6d8d06b00c1ffda03b81419ebdd21e945acaedb1f9d71b690896@ec2-54-83-1-101.compute-1.amazonaws.com:5432/dcidfrniod064s?ssl=true"
-
-/*
-const path = require("path");
-const PORT = process.env.PORT || 5000;
-require("dotenv").config();
-//const connectionString = process.env.DATABASE_URL;
-
-const connectionString = process.env.DATABASE_URL || "postgres://qofbggsapgbgtx:362d864eeb0f6d8d06b00c1ffda03b81419ebdd21e945acaedb1f9d71b690896@ec2-54-83-1-101.compute-1.amazonaws.com:5432/dcidfrniod064s?ssl=true"
-
-const pool = new Pool({ connectionString: connectionString });
-
+var request = require("request");
 var app = express();
 
 app.use(express.static("public"));
@@ -31,38 +18,32 @@ app.get("/home", function() {
   console.log("recived a request for the home page");
 });
 
- app.get("/getPerson", function(req, res) {
+app.listen(5005, function() {
+  console.log("server open. Listening on port 5005");
+});
 
-    var sql = "SELECT first_name FROM person where id = $1";
+app.get("/cardSearch", function(req, res) {
+  console.log("recived a request to return the page to search for cards");
 
+  res.render("pages/cardSearch");
+  res.end();
+});
 
-    pool.query(sql, [req], function(err, result) {
-        // If an error occurred...
-        if (err) {
-          console.log("Error in query: ");
-          console.log(err);
-        }
-      
-        // Log this to the console for debugging purposes.
-        console.log("Back from DB with result:");
-        console.log(result.rows);
-        res.send(result.rows);
-      })
-
-}); */
-
-router.get('/getperson', function(req, res, next) {
-    pg.connect(conString, function(err, client, done) {
-      if (err) {
-        return console.error('error fetching client from pool', err);
-      }
-      console.log("connected to database");
-      client.query('SELECT * FROM person', function(err, result) {
-        done();
-        if (err) {
-          return console.error('error running query', err);
-        }
-        res.send(result);
-      });
-    });
+app.get("/scryfall/cardquery", function(req, res) {
+  console.log("recived a request to get card list");
+  console.log(req.query);
+  var url = "https://api.scryfall.com/cards/search?q=" + req.query["cardnamesearch"];
+  console.log("url: " + url);
+  
+  request(url, function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var importedJSON = JSON.parse(body);
+      //console.log(importedJSON);
+      res.json(importedJSON);
+      res.end();
+    }
+    if (error) {
+      console.log("error in query");
+    }
   });
+});
